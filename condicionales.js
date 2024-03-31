673efdc7d66d2812219e9fd50b67a204563b42ac
Ale <alexlab84@hotmail.com>
2024-03-31 15:43:18 +0200

    Add exercice rock, paper and scissors

---
+++ condicionales.js
@@ -0,0 +1,45 @@
+/* let edad = 18;
+if (edad === 18) {
+    return "Puedes votar, es tu primera vez"    
+} else if (edad > 18) {
+    return "Puedes votar, eres veterano en esto"
+} else {
+    return "No puedes votar, votaras cuando seas mayor de 18 años"
+}
+
+
+//Operador ternario
+
+condition ? true : false;
+
+let numero = 1;
+
+let resultado = numero === 1 ? "Soy un uno" : "No soy un uno"  
+console.log(resultado) */
+
+
+let piedra = "piedra" || "Piedra" || "PIEDRA"; 
+let papel = "papel" || "Papel" || "PAPEL";
+let tijeras = "tijeras" || "Tijeras" || "TIJERA"
+
+function juegoPiedraPapelTijera(jugador, pc) {
+    if (jugador === piedra && pc === papel) {
+       return "Has perdido" 
+    } else if (jugador === piedra && pc === tijeras) {
+        return "Has ganado"
+    } else if (jugador === piedra && pc === piedra) {
+       return "Has empatado" 
+    } else if (jugador === papel && pc === piedra) {
+        return "Has ganado"
+    } else if (jugador === papel && pc === papel){
+        return "Has empatado" 
+    } else if (jugador === papel && pc === tijeras) {
+        return "Has perdido"
+    } else if (jugador === tijeras && pc === piedra) {
+        return "Has perdido"
+    } else if (jugador === tijeras && pc === papel) {
+        return "Has ganado"
+    } else if (jugador === tijeras && pc === tijeras) {
+        return "Has empatado" 
+    }
+}
